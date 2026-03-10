5e8e0ccb274c0c400ec09de4899f6e45dd0f0bf2
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-10 11:15:27 +0500

    bitwise operations

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,17 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello Backend Developer");
+//    }
+//}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Backend Developer");
+        int a = 5, b = 3;
+        System.out.println("Bitwise Operations:");
+        System.out.println("a & b = " + (a & b));
+        System.out.println("a | b = " + (a | b));
+        System.out.println("a ^ b = " + (a ^ b));
     }
 }
