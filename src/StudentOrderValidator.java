5313906d1932d4d15020cf3648ce352178586e48
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-11 10:17:23 +0500

    Learn java

---
+++ src/StudentOrderValidator.java
@@ -0,0 +1,26 @@
+public class StudentOrderValidator
+{
+    static void main(String[] args) {
+        checkAll();
+
+    }
+    static void checkAll() {
+        checkCityRegister();
+        checkWedding();
+        checkStudent();
+        checkChildren();
+
+    }
+    static void checkCityRegister() {
+        System.out.println("City is running");
+    }
+    static void checkWedding() {
+        System.out.println("Wedding zapushen");
+    }
+    static void checkChildren() {
+        System.out.println("Children check is running");
+    }
+    static void checkStudent() {
+        System.out.println("Students are checking");
+    }
+}
