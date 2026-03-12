a1abb6ee53d83c03ca8ca736be71cc71a0b1c326
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-12 11:37:55 +0500

    Arifmethic methods

---
+++ src/BasicArithmetic.java
@@ -0,0 +1,44 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println("Hello Backend Developer");
+//    }
+//}
+
+//public class Main {
+//    public static void main(String[] args) {
+//        int a = 5, b = 3;
+//        System.out.println("Bitwise Operations:");
+//        System.out.println("a & b = " + (a & b));
+//        System.out.println("a | b = " + (a | b));
+//        System.out.println("a ^ b = " + (a ^ b));
+//    }
+//}
+
+public class BasicArithmetic {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 4;
+
+        // Qo'shish
+        int sum = a + b;
+        System.out.println(a + " + " + b + " = " + sum); // 15 + 4 = 19
+
+        // Ayirish
+        int difference = a - b;
+        System.out.println(a + " - " + b + " = " + difference); // 15 - 4 = 11
+
+        // Ko'paytirish
+        int product = a * b;
+        System.out.println(a + " * " + b + " = " + product); // 15 * 4 = 60
+
+        // Bo'lish (butun sonlarda)
+        int quotient = a / b;
+        System.out.println(a + " / " + b + " = " + quotient); // 15 / 4 = 3
+
+        // Qoldiq
+        int remainder = a % b;
+        System.out.println(a + " % " + b + " = " + remainder); // 15 % 4 = 3
+    }
+}
