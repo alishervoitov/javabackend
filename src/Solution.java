822a4c9bbd9e96687209b36dc63e9f9f1092bd58
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-17 20:09:30 +0500

    Delete some codes

--- src/Solution.java
+++ src/Solution.java
@@ -6,15 +6,6 @@
 //    }
 //}
 
-//public class Main {
-//    public static void main(String[] args) {
-//        int a = 5, b = 3;
-//        System.out.println("Bitwise Operations:");
-//        System.out.println("a & b = " + (a & b));
-//        System.out.println("a | b = " + (a | b));
-//        System.out.println("a ^ b = " + (a ^ b));
-//    }
-//}
 
 //public class BasicArithmetic {
 //    public static void main(String[] args) {
