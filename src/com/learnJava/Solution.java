d7ae1402d1a0bb31d695069def310f030a21cf10
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-20 23:18:58 +0500

    palindrom

--- src/com/learnJava/Solution.java
+++ src/com/learnJava/Solution.java
@@ -215,55 +215,90 @@
 //    }
 //}
 
-package com.learnJava;
+//package com.learnJava;
+//
+//public class ArrangeArray {
+//
+//    private int [] array={2,5,7,8,1,6,9};
+//    private int len=array.length;
+//    public static void main(String [] args)
+//    {
+//        ArrangeArray a=new ArrangeArray();
+//        a.print();
+//        a.arrange();
+//        a.print();
+//
+//    }
+//    public  void print()
+//    {
+//        for(int i=0;i<array.length;i++)
+//        {
+//            System.out.print(array[i] + " ");
+//
+//        }
+//        System.out.println();
+//    }
+//    public void arrange()
+//    {
+//        int oddinx=1;
+//        int evenidx=0;
+//        while(true)
+//        {
+//            while(evenidx<len && array[evenidx]%2==0)
+//            {
+//                evenidx+=2;
+//            }
+//            while(oddinx<len && array[oddinx]%2==1)
+//            {
+//                oddinx+=2;
+//            }
+//            if (evenidx < len && oddinx < len)
+//                swap (evenidx, oddinx);
+//            else
+//                break;
+//
+//        }
+//
+//    }
+//    public void swap(int a,int b)
+//    {
+//        int tmp=array[b];
+//        array[b]=array[a];
+//        array[a]=tmp;
+//    }
+//}
 
-public class ArrangeArray {
 
-    private int [] array={2,5,7,8,1,6,9};
-    private int len=array.length;
-    public static void main(String [] args)
-    {
-        ArrangeArray a=new ArrangeArray();
-        a.print();
-        a.arrange();
-        a.print();
+public class Solution {
 
-    }
-    public  void print()
-    {
-        for(int i=0;i<array.length;i++)
-        {
-            System.out.print(array[i] + " ");
+    int len = 0;
+    String res = "";
 
+    public String longestPalindrome(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            palindrome(s, i, i);
+            palindrome(s, i, i + 1);
         }
-        System.out.println();
+        return res;
     }
-    public void arrange()
-    {
-        int oddinx=1;
-        int evenidx=0;
-        while(true)
-        {
-            while(evenidx<len && array[evenidx]%2==0)
-            {
-                evenidx+=2;
-            }
-            while(oddinx<len && array[oddinx]%2==1)
-            {
-                oddinx+=2;
-            }
-            if (evenidx < len && oddinx < len)
-                swap (evenidx, oddinx);
-            else
-                break;
 
+    void palindrome(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
         }
 
+        int currentLen = r - l - 1;
+
+        if (currentLen > len) {
+            res = s.substring(l + 1, r);
+            len = currentLen;
+        }
     }
-    public void swap(int a,int b)
-    {
-        int tmp=array[b];
-        array[b]=array[a];
-        array[a]=tmp;
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "babad";
+        System.out.println(sol.longestPalindrome(s));
     }
 }
