9f31467e0e9c5a1c04c0a35c537e31019c354956
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-22 20:52:41 +0500

    Leetcode

--- src/com/learnJava/Solution.java
+++ src/com/learnJava/Solution.java
@@ -304,25 +304,42 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //}
 
 
-public class Solution {
-    public boolean isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
+//public class Solution {
+//    public boolean isPalindrome(int x) {
+//        if (x < 0) {
+//            return false;
+//        }
+//
+//        long div = 1;
+//        while (x >= 10 * div) {
+//            div *= 10;
+//        }
+//
+//        while (x != 0) {
+//            if (x / div != x % 10) {
+//                return false;
+//            }
+//            x = (int) (x % div) / 10;
+//            div /= 100;
+//        }
+//
+//        return true;
+//    }
+//}
 
-        long div = 1;
-        while (x >= 10 * div) {
-            div *= 10;
-        }
-
-        while (x != 0) {
-            if (x / div != x % 10) {
-                return false;
+class Solution {
+    public String intToRoman(int num) {
+        String[] sym = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
+        int[] val = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+        StringBuilder res = new StringBuilder();
+        int idx = val.length - 1;
+        while(num > 0 && idx >= 0){
+            if(num < val[idx]) idx -= 1;
+            else{
+                num -= val[idx];
+                res.append(sym[idx]);
             }
-            x = (int) (x % div) / 10;
-            div /= 100;
         }
-
-        return true;
+        return res.toString();
     }
 }
