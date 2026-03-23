44a4bffa5c14eb69e0fb8784e3ab824337bd50f5
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-23 19:52:51 +0500

    Edit the code

--- src/com/learnJava/Solution.java
+++ src/com/learnJava/Solution.java
@@ -327,19 +327,33 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //    }
 //}
 
-class Solution {
+public class Solution {
+
     public String intToRoman(int num) {
         String[] sym = {"I", "IV", "V", "IX", "X", "XL", "L", "XC", "C", "CD", "D", "CM", "M"};
         int[] val = {1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900, 1000};
+
         StringBuilder res = new StringBuilder();
         int idx = val.length - 1;
-        while(num > 0 && idx >= 0){
-            if(num < val[idx]) idx -= 1;
-            else{
+
+        while (num > 0 && idx >= 0) {
+            if (num < val[idx]) {
+                idx--;
+            } else {
                 num -= val[idx];
                 res.append(sym[idx]);
             }
         }
+
         return res.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int num = 1994;
+        String result = sol.intToRoman(num);
+
+        System.out.println("Roman: " + result);
     }
 }
