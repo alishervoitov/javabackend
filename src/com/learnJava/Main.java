6c2a6710cbfb9426ed5d71a16b9d76cd5072c062
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-25 15:10:31 +0500

    example for WHILE in java

--- src/com/learnJava/Main.java
+++ src/com/learnJava/Main.java
@@ -497,31 +497,51 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //    }
 //}
 
+//import java.util.Scanner;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        String choice;
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("What do you prefer?");
+//        System.out.println("1 a candy");
+//        System.out.println("2 chocolate");
+//        System.out.println("3 a cake ");
+//        System.out.println("Enter your choice:");
+//        choice = sc.nextLine();
+//        switch(choice) {
+//            case "1":
+//                System.out.println("You need 5 dollars.");
+//                break;
+//            case "2":
+//                System.out.println("You need 10 dollars.");
+//                break;
+//            case "3":
+//                System.out.println("You need 15 dollars.");
+//                break;
+//            default:
+//                System.out.println("Invalid choice");
+//                break;
+//        }
+//    }
+//}
+
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String choice;
         Scanner sc = new Scanner(System.in);
-        System.out.println("What do you prefer?");
-        System.out.println("1 a candy");
-        System.out.println("2 chocolate");
-        System.out.println("3 a cake ");
-        System.out.println("Enter your choice:");
-        choice = sc.nextLine();
-        switch(choice) {
-            case "1":
-                System.out.println("You need 5 dollars.");
-                break;
-            case "2":
-                System.out.println("You need 10 dollars.");
-                break;
-            case "3":
-                System.out.println("You need 15 dollars.");
-                break;
-            default:
-                System.out.println("Invalid choice");
-                break;
+        int amount;
+        int cost = 2;
+        int count = 0;
+        System.out.println("How much money does Mary have? ");
+        amount = sc.nextInt();
+        int sum = 0;
+        while((amount-sum) >= cost) {
+            count++;
+            sum += cost;
         }
+        System.out.println("Mary can buy " + count + " candy(es).");
     }
 }
