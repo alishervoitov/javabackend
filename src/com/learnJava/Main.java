7e3003039326658f903380350aadf73a17fa30bf
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-25 14:22:28 +0500

    Epam example

--- src/com/learnJava/Main.java
+++ src/com/learnJava/Main.java
@@ -436,38 +436,50 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //    }
 //}
 
+//public class Main {
+//    public static void main (String[] args){
+//        int a = 9;
+//        int b = 4;
+//
+//        if ( a < b ) {
+//            System.out.println("This will never print because it is false." );
+//        }
+//        if ( a <= b ) {
+//            System.out.println("This will never print because it is false." );
+//        }
+//        if ( a == 4 ) {
+//            System.out.println("This will never print because it is false." );
+//        }
+//        if ( b == 4 ) {
+//            System.out.println("The expression (b == 4) is true." );
+//        }
+//        if ( a != b ) {
+//            System.out.println("The expression (a != b) is true." );
+//        }
+//        if ( a > b ) {
+//            System.out.println("The expression (a > b) is true." );
+//        }
+//        if ( a >= b ) {
+//            System.out.println("The expression (a >= b) is true." );
+//        }
+//        if ( (a != b) && (a > b) ) {
+//            System.out.println("This will be printed because both conditions are true." );
+//        }
+//        if ( (a != b) || (a < b) || ( a == 4 ) ) {
+//            System.out.println("This will be printed because one of the conditions is true." );
+//        }
+//
+//    }
+//}
+
 public class Main {
-    public static void main (String[] args){
-        int a = 9;
-        int b = 4;
-
-        if ( a < b ) {
-            System.out.println("This will never print because it is false." );
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        if (a == b) {
+            System.out.println("a and b are equal");
+        } else {
+            System.out.println("a and b are not equal");
         }
-        if ( a <= b ) {
-            System.out.println("This will never print because it is false." );
-        }
-        if ( a == 4 ) {
-            System.out.println("This will never print because it is false." );
-        }
-        if ( b == 4 ) {
-            System.out.println("The expression (b == 4) is true." );
-        }
-        if ( a != b ) {
-            System.out.println("The expression (a != b) is true." );
-        }
-        if ( a > b ) {
-            System.out.println("The expression (a > b) is true." );
-        }
-        if ( a >= b ) {
-            System.out.println("The expression (a >= b) is true." );
-        }
-        if ( (a != b) && (a > b) ) {
-            System.out.println("This will be printed because both conditions are true." );
-        }
-        if ( (a != b) || (a < b) || ( a == 4 ) ) {
-            System.out.println("This will be printed because one of the conditions is true." );
-        }
-
     }
 }
