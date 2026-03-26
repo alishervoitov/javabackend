c6fbf08ee551af8eb6f12f76c8d8fe01c4104f88
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-26 15:37:59 +0500

    recursion in java

--- src/com/learnJava/Main.java
+++ src/com/learnJava/Main.java
@@ -617,15 +617,35 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //    }
 //}
 
-public class Func {
-    public static void main(String[] args) {
-        int x;
-        x = method(3);
-        System.out.println(x);
-    }
+//public class Func {
+//    public static void main(String[] args) {
+//        int x;
+//        x = method(3);
+//        System.out.println(x);
+//    }
+//
+//    public static int method(int x) {
+//        if (x <= 0) return x;
+//        else return x = x * method(x-1);
+//    }
+//}
 
-    public static int method(int x) {
-        if (x <= 0) return x;
-        else return x = x * method(x-1);
+import java.util.Scanner;
+
+public class Main {
+    static int calculateGCD(int num1, int num2) {
+        if (num2 != 0)
+            return calculateGCD(num2, num1 % num2);
+        else
+            return num1;
+    }
+    public static void main(String[] args) {
+        System.out.print("Enter the first positive integer value: ");
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        System.out.print("Enter the second positive integer value: ");
+        int num2 = sc.nextInt();
+        System.out.print("The greatest common divisor of " + num1 + " and " + num2 + " is " + calculateGCD(num1, num2));
+        sc.close();
     }
 }
