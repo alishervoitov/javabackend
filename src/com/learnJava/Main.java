545b104384469681a3d24d33ad1b82119f88a915
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-03-27 16:03:14 +0500

    ...'

--- src/com/learnJava/Main.java
+++ src/com/learnJava/Main.java
@@ -630,22 +630,38 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //    }
 //}
 
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class Main {
+//    static int calculateGCD(int num1, int num2) {
+//        if (num2 != 0)
+//            return calculateGCD(num2, num1 % num2);
+//        else
+//            return num1;
+//    }
+//    public static void main(String[] args) {
+//        System.out.print("Enter the first positive integer value: ");
+//        Scanner sc = new Scanner(System.in);
+//        int num1 = sc.nextInt();
+//        System.out.print("Enter the second positive integer value: ");
+//        int num2 = sc.nextInt();
+//        System.out.print("The greatest common divisor of " + num1 + " and " + num2 + " is " + calculateGCD(num1, num2));
+//        sc.close();
+//    }
+//}
 
 public class Main {
-    static int calculateGCD(int num1, int num2) {
-        if (num2 != 0)
-            return calculateGCD(num2, num1 % num2);
-        else
-            return num1;
-    }
     public static void main(String[] args) {
-        System.out.print("Enter the first positive integer value: ");
-        Scanner sc = new Scanner(System.in);
-        int num1 = sc.nextInt();
-        System.out.print("Enter the second positive integer value: ");
-        int num2 = sc.nextInt();
-        System.out.print("The greatest common divisor of " + num1 + " and " + num2 + " is " + calculateGCD(num1, num2));
-        sc.close();
+        method2();
     }
+
+    public static void method1() {
+        System.out.print("Hello, ");
+        method2();
+    }
+
+    public static void method2() {
+        System.out.print("world.");
+    }
+
 }
