e81b29115c8cfb832c7f9de2fda5f846273a53ba
Alisher <114944816+alishervoitov@users.noreply.github.com>
2026-04-10 22:38:17 +0500

    ....

--- src/com/learnJava/ArrayRectangle.java
+++ src/com/learnJava/ArrayRectangle.java
@@ -817,8 +817,6 @@ package com.learnJava;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/>
 //}
 
 
-
-import java.util.Scanner;
 //
 //class Node {
 //    int dataElement;
@@ -1987,75 +1985,75 @@ import java.util.Scanner;
 //    }
 //}
 
-//
-//import java.util.Objects;
-//
-//public class Rectangle {
-//    private double sideA;
-//    private double sideB;
-//
-//    // Ikki parametrli konstruktor
-//    public Rectangle(double a, double b) {
-//        this.sideA = a;
-//        this.sideB = b;
-//    }
-//
-//    // Kvadrat uchun bitta parametrli konstruktor
-//    public Rectangle(double side) {
-//        this(side, side);
-//    }
-//
-//    // Parametrsiz konstruktor (A=4, B=3)
-//    public Rectangle() {
-//        this.sideA = 4;
-//        this.sideB = 3;
-//    }
-//
-//    public double getSideA() {
-//        return sideA;
-//    }
-//
-//    public double getSideB() {
-//        return sideB;
-//    }
-//
-//    public double area() {
-//        return sideA * sideB;
-//    }
-//
-//    public double perimeter() {
-//        return 2 * (sideA + sideB);
-//    }
-//
-//    public boolean isSquare() {
-//        return sideA == sideB;
-//    }
-//
-//    public void replaceSides() {
-//        double temp = sideA;
-//        sideA = sideB;
-//        sideB = temp;
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        Rectangle rectangle = (Rectangle) o;
-//        return Double.compare(rectangle.sideA, sideA) == 0 &&
-//                Double.compare(rectangle.sideB, sideB) == 0;
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(sideA, sideB);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Rectangle{" + "sideA=" + sideA + ", sideB=" + sideB + '}';
-//    }
-//}
+
+import java.util.Objects;
+
+public class Rectangle {
+    private double sideA;
+    private double sideB;
+
+    // Ikki parametrli konstruktor
+    public Rectangle(double a, double b) {
+        this.sideA = a;
+        this.sideB = b;
+    }
+
+    // Kvadrat uchun bitta parametrli konstruktor
+    public Rectangle(double side) {
+        this(side, side);
+    }
+
+    // Parametrsiz konstruktor (A=4, B=3)
+    public Rectangle() {
+        this.sideA = 4;
+        this.sideB = 3;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double area() {
+        return sideA * sideB;
+    }
+
+    public double perimeter() {
+        return 2 * (sideA + sideB);
+    }
+
+    public boolean isSquare() {
+        return sideA == sideB;
+    }
+
+    public void replaceSides() {
+        double temp = sideA;
+        sideA = sideB;
+        sideB = temp;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(rectangle.sideA, sideA) == 0 &&
+                Double.compare(rectangle.sideB, sideB) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sideA, sideB);
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" + "sideA=" + sideA + ", sideB=" + sideB + '}';
+    }
+}
 
 
 public class ArrayRectangles {
